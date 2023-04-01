85a8f814c6c65cf90e635fd5fed3d789e726e729
efearon1 <125225643+efearon1@users.noreply.github.com>
2023-04-01 12:15:29 +0100

    Create env.sh

---
+++ config/env.sh
@@ -0,0 +1,14 @@
+export KDB_HOME=~/advancedKDB
+export TICK_DIR=${KDB_HOME}/tick
+export LOG_DIR=${KDB_HOME}/logs
+export TP_LOG_DIR=${LOG_DIR}/tpLogs
+export CSV_DIR=${KDB_HOME}/csv
+
+export TP_PORT=6210
+export RDB1_PORT=6201
+export RDB2_PORT=6202
+export FH_PORT=6204
+export CEP_PORT=6204
+export HDB_PORT=6212
+
+export TP_TIMER=100
