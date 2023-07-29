931adc7340ac28d799191a47a00de3d3794d5b54
efearon1 <125225643+efearon1@users.noreply.github.com>
2023-07-29 14:49:40 +0100

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,11 @@
 source ~/advancedKDB/config/env.sh
 cd ${KDB_HOME}
 
-TICK_START="nohup ~/q/${Q_OS}/q ${KDB_HOME}/tick.q sym ${TP_LOG_DIR} -p ${TP_PORT} </dev/null >> ${LOG_DIR}/tick.log 2>&1 &"
-RDB1_START="nohup ~/q/${Q_OS}/q ${TICK_DIR}/rdb1.q -p ${RDB1_PORT} -tp ${TP_PORT} -hdb ${HDB_PORT} -tabs trade quote </dev/null >> ${LOG_DIR}/rdb1.log 2>&1 &"
-RDB2_START="nohup ~/q/${Q_OS}/q ${TICK_DIR}/rdb2.q -p ${RDB2_PORT} -tp ${TP_PORT} -hdb ${HDB_PORT} -tabs aggregation </dev/null >> ${LOG_DIR}/rdb2.log 2>&1 &"
-FEED_START="nohup ~/q/${Q_OS}/q ${TICK_DIR}/feed.q -p ${FH_PORT} </dev/null >> ${LOG_DIR}/feed.log 2>&1 &"
-CEP_START="nohup ~/q/${Q_OS}/q ${TICK_DIR}/cep.q -p ${CEP_PORT} -tp ${TP_PORT} -tabs trade quote </dev/null >> ${LOG_DIR}/cep.log 2>&1 &"
+TICK_START="nohup q ${KDB_HOME}/tick.q sym ${TP_LOG_DIR} -p ${TP_PORT} >> ${LOG_DIR}/tick.log 2>&1 &"
+RDB1_START="nohup q ${TICK_DIR}/rdb1.q -p ${RDB1_PORT} -tp ${TP_PORT} -hdb ${HDB_PORT} -tabs trade quote >> ${LOG_DIR}/rdb1.log 2>&1 &"
+RDB2_START="nohup q ${TICK_DIR}/rdb2.q -p ${RDB2_PORT} -tp ${TP_PORT} -hdb ${HDB_PORT} -tabs aggregation >> ${LOG_DIR}/rdb2.log 2>&1 &"
+FEED_START="nohup q ${TICK_DIR}/feed.q -p ${FH_PORT} >> ${LOG_DIR}/feed.log 2>&1 &"
+CEP_START="nohup q ${TICK_DIR}/cep.q -p ${CEP_PORT} -tp ${TP_PORT} -tabs trade quote >> ${LOG_DIR}/cep.log 2>&1 &"
 
 
 echo "Do you wish to start all processes?, (y/n)"
