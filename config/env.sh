eae0473d7b5b58d22717be93de0cdda2d71e254c
efearon1 <125225643+efearon1@users.noreply.github.com>
2023-08-05 15:24:19 +0100

    Update env.sh

--- config/env.sh
+++ config/env.sh
@@ -4,12 +4,16 @@ export LOG_DIR=${KDB_HOME}/logs
 export TP_LOG_DIR=${LOG_DIR}/tpLogs
 export CSV_DIR=${KDB_HOME}/csv
 
+//Exercise 1 ports
 export TP_PORT=6210
 export RDB1_PORT=6201
 export RDB2_PORT=6202
 export FH_PORT=6204
 export CEP_PORT=6205
 export HDB_PORT=6212
+
+//Exercise 3 ports
+export WB_PORT=6215
 
 export TP_TIMER=100
 export Q_OS=l64
