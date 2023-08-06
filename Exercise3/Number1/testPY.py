a21b12ad40d9f1fd4848cc5bb11e8821dcfa09c0
efearon1 <125225643+efearon1@users.noreply.github.com>
2023-08-06 10:50:38 +0100

    Update testPY.py

--- Exercise3/Number1/testPY.py
+++ Exercise3/Number1/testPY.py
@@ -44,30 +44,20 @@ def convertTimeToLong(timeValue):
     
 # Read in csv file(s) into python dataframe and publish to TP
 def csvReadandPublish():
-    with open(/home/efearon_kx_com/csv/trade.csv) as csv_file:
+    with open("/home/efearon_kx_com/csv/trade.csv") as csv_file:
         csv_reader = csv.reader(csv_file,delimiter=',')
         row_count = 0
 
         for row in csv_reader:
             if row_count > 0:
-                '''
-                    Trade Table types
+                row[0] = K.timespan(convertTimeToLong(row[0]))
+                row[2] = K.float(int(row[2]))
+                row[3] = K.int(int(row[3]))
                         
-                    time - timespan (.z.N) --> we will cast this to a long value which will be casted to timespan datatype on kdb
-                    sym - symbol (no casting required) --> python str are automatically casted to kdb sym type
-                    price - float
-                    size - int
-                    '''
-                    row[0] = K.timespan(convertTimeToLong(row[0]))
-                    row[2] = K.float(int(row[2]))
-                    row[3] = K.int(int(row[3]))
-                        
-                    #Send the converted row values to the TP process through the handle
-                    handle(('.u.upd','trade',(row[0],row[1],row[2],row[3])))
-                row_count += 1
+                #Send the converted row values to the TP process through the handle
+                handle(('.u.upd','trade',(row[0],row[1],row[2],row[3])))
+            row_count += 1
                 
-
-
 print ("Starting Python API Script")
 #argCapture()
 #csvFind()
